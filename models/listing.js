b15ae9ad1545873fae20e7bf0d42e3e44d332e1b
rajmakvana143 <rajmakvana143e@gmail.com>
2024-08-06 16:48:19 +0530

    modeify the model

--- models/listing.js
+++ models/listing.js
@@ -5,13 +5,16 @@ const mongoose = require('mongoose');
 const listingSchema = new mongoose.Schema({
     title : {
         type : String,
-        required : true
+        required : true,
     },
     description : String,
     image : {
-        type : String,
+        filename : String,
+        url : {
+            type : String,
         default : "https://images.unsplash.com/photo-1696087636176-b689cb60b9b1?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set : (v) => v === "" ? "https://images.unsplash.com/photo-1696087636176-b689cb60b9b1?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
+        }
     },
     price : Number,
     location : String,
