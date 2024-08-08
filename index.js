d2e0d02e2773f22b1c8337fd76c6bf4db5a55f39
rajmakvana143 <rajmakvana143e@gmail.com>
2024-08-08 13:44:30 +0530

    all routes completed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+var methodOverride = require('method-override')
 const port = 3000;
 
 
@@ -28,6 +29,9 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views" , path.join(__dirname,"views"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(methodOverride('_method'));
 
 app.listen(port, () => {
   console.log(`port is listening on ${port} `);
@@ -37,11 +41,42 @@ app.get("/", (req, res) => {
   res.send("root is workking");
 });
 
-// index.route 
-app.get("/listings" , async (req , res) => {
+// index route 
+app.get("/Listings" , async (req , res) => {
   const allListings  = await Listing.find({});
   res.render("listings/index.ejs" , {allListings});
 });
 
+//  new route
+app.get("/Listings/new" , (req , res) => {
+  res.render("listings/new.ejs")
+});
 
+// show route 
+app.get("/Listings/:id" , async (req , res) => {
+  let {id} = req.params ;
+  let Listings = await Listing.findById(id);
+  res.render("listings/show.ejs" , {Listings})
+});
+
+// create route 
+app.post("/Listings" , async (req , res) => {
+  let newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+})
+
+// edite route 
+
+app.get("/Listings/:id/edit" , async (req ,res) =>{
+  let {id} = req.params;
+  let data = await Listing.findById(id);
+  res.render("listings/edit.ejs" , {data})
+});
+
+app.put("/Listings/:id" , async (req , res) => {
+  let {id} = req.params;
+  let update  = await Listing.findByIdAndUpdate(id , {...req.body.listing});
+  res.redirect("/Listings");
+});
 
