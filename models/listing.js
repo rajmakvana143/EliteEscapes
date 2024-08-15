be143509dddf6fa6a99f98f8c8b36812b9f2ac84
rajmakvana143 <rajmakvana143e@gmail.com>
2024-08-15 09:46:12 +0530

    changed module

--- models/listing.js
+++ models/listing.js
@@ -1,30 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Create a new Schema
 
 const listingSchema = new mongoose.Schema({
-    title : {
-        type : String,
-        required : true,
-    },
-    description : String,
-    image : {
-        filename : {
-            type : String,
-            default : "tourism"
-        },
-        url : {
-            type : String,
-        default : "https://images.unsplash.com/photo-1696087636176-b689cb60b9b1?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set : (v) => v === "" ? "https://images.unsplash.com/photo-1696087636176-b689cb60b9b1?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
-        }
-    },
-    price : Number,
-    location : String,
-    country : String
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1696087636176-b689cb60b9b1?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1696087636176-b689cb60b9b1?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
 
-const Listing = new mongoose.model('Listing' , listingSchema)
+const Listing = new mongoose.model("Listing", listingSchema);
 
 module.exports = Listing;
-
